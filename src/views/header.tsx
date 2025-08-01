d093b01cb2ccfb5bb49c68556c3aac2cf98d855c
pzzyf <2279948211@qq.com>
2025-08-01 14:12:58 +0800

    header

--- src/views/header.tsx
+++ src/views/header.tsx
@@ -1,8 +1,10 @@
+import { GithubIcon } from '@/components/icons'
 import { ThemeSwitche } from '@/components/theme-switch'
 
 export function Header() {
   return (
-    <div className="h-[90px]">
+    <div className="h-[50px] flex items-center justify-end gap-2 border border-solid border-[#d9d9d9] dark:border-[#ffffff20]">
+      <GithubIcon className="cursor-pointer" onClick={() => window.open('https://github.com/pzzyf/a-designer', '_blank')} />
       <ThemeSwitche />
     </div>
   )
